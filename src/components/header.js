42db8f345eb94c1cb19edefb45414fb48d73ae3c
Demi Hayashi <demi.h@me.com>
2022-11-03 15:41:32 -0700

    update css in header & hero

--- src/components/header.js
+++ src/components/header.js
@@ -4,17 +4,17 @@ import { Link } from 'gatsby';
 const Header = () => (
     <header>
         <div>
-            <div className="fixed top-0 z-50 w-full flex justify-between items-center p-4 bg-[#ddd]">
+            <div className="fixed top-0 z-50 w-full flex justify-between items-center p-4 bg-gradient-to-b from-white via-[#ffffff88]">
                 <div className="Logo">
-                    <Link to="/">TOKIO RAMEN</Link>
+                    <Link to="/" className="text-2xl text-[#fe4039] font-semibold uppercase">Tokio</Link>
                 </div>
 
                 <div className="navigation">
                     <nav>
-                        <Link to="/" className="mx-2">Home</Link>
-                        <Link to="/our-history" className="mx-2">Our History</Link>
-                        <Link to="/menu" className="mx-2">Menu</Link>
-                        <Link to="/contact" className="ml-2">Contact</Link>
+                        <Link to="/" className="mr-4 hover:text-[#fe4039]">Home</Link>
+                        <Link to="/our-history" className="mx-4 hover:text-[#fe4039]">Our History</Link>
+                        <Link to="/menu" className="mx-4 hover:text-[#fe4039]">Menu</Link>
+                        <Link to="/contact" className="ml-4 hover:text-[#fe4039]">Contact</Link>
                     </nav>
                 </div>
             </div>
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
 const Hero = () => {
@@ -7,9 +6,9 @@ const Hero = () => {
     <div className="relative flex align-center h-screen">
         <div className="container">
             <div className="row">
-                <p className="z-[9] w-full absolute left-0 top-[50%] translate-y-2/4 text-6xl font-bold text-center uppercase tracking-[.3em] drop-shadow-lg">Ramen done right.</p>
+                <p className="z-[9] w-full absolute left-0 top-[50%] translate-y-[-50%] pl-8 text-7xl text-gray-300 mix-blend-color-dodge font-medium uppercase tracking-[.2em] drop-shadow-lg">Ramen done right.</p>
                 <div className="hero">
-                    <StaticImage src="../images/hero.png" alt="Welcome to Tokio Ramen" className="fixed object-cover h-full w-full z-0" />
+                    <StaticImage src="../images/hero.png" alt="Welcome to Tokio Ramen" className="fixed top-[65px] object-cover h-full w-full z-0 saturate-[1.25]" />
                 </div>
             </div>
         </div>
