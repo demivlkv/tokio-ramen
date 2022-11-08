8c52fa2edf155b28f9cf421f9188b7bd4c528669
Demi Hayashi <demi.h@me.com>
2022-11-08 13:58:18 -0800

    update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,23 +1,41 @@
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const About = () => (
   <>
-    <div className="w-full my-16">
-      <div className="max-w-screen-md mx-auto px-8 flex flex-col justify-center h-full">
-        <h2 className="pb-4 font-medium text-2xl text-[#fe4039] uppercase tracking-wide">About Us</h2>
-        <p className="py-4">
-          After immigrating to the United States from Japan in the 1990s, Chef Hiro owned and operated multiple restaurants over a 20-year period. Tokio Ramen was a dream project that came to fruition in which Chef Hiro wanted to offer a Japanese culinary staple known as ramen that is gaining attention with the word quickly spreading. Ramen is, put simply, Japanese style noodles bathed in broth with complementing toppings.
-        </p>
-        <p className="py-4">
-          The menu is intentionally scaled down so that focus is directed at delivering the freshest, best quality ramen. This is the place you can come to and experience an impressionable taste of Japan without a plane ticket. Come join us for a delicious bowl of ramen with a side of steaming gyoza and a refreshing cold Japanese beverage. This is the real no-frills deal with no fusion element to it, because we prefer authenticity like you do.
-        </p>
-        <p className="py-4">
-          We are firm believers in acknowledging and giving credit where it's due. With that, we'd like to let our utmost gratitude be known to those who've helped us along the way:
-        </p>
-        <p className="pt-4 text-center">
-          City of Boynton Beach (with distinguished praise for the CRA!)  |  Catoe & Son Plumbing  |  Flameguard Fire Equipment | LifeSafety Management  |  Chip Brown of AMJ  |  Challenger Operations Inc
-        </p>
+    <div className="w-full h-screen">
+      <StaticImage src="../images/top-border.png" alt="" className="w-full" />
+      <div className="bg-[#222] py-10">
+        <h2 className="pb-8 font-normal text-2xl text-[#fe4039] text-center uppercase tracking-widest">Our History</h2>
+        <div className="max-w-screen-lg w-full grid px-4 sm:grid-cols-2 gap-8 mx-auto text-white">
+          <div className="flex justify-center items-center">
+            <StaticImage src="../images/our-history.png" alt="Tokio Ramen Owners" className="drop-shadow-md" />
+          </div>
+          <div>
+            <p className="pb-2">
+              After immigrating to the United States from Japan in the 1990s, Chef Hiro owned and operated multiple restaurants over a 20-year period. Tokio Ramen was a dream project that came to fruition in which Chef Hiro wanted to offer a Japanese culinary staple known as ramen that is gaining attention with the word quickly spreading. Ramen is, put simply, Japanese style noodles bathed in broth with complementing toppings.
+            </p>
+            <p className="py-2">
+              The menu is intentionally scaled down so that focus is directed at delivering the freshest, best quality ramen. This is the place you can come to and experience an impressionable taste of Japan without a plane ticket. Come join us for a delicious bowl of ramen with a side of steaming gyoza and a refreshing cold Japanese beverage. This is the real no-frills deal with no fusion element to it, because we prefer authenticity like you do.
+            </p>
+          </div>
+        </div>
+        <div className="max-w-screen-md w-full mx-auto text-white px-4">
+          <p className="py-2">
+            We are firm believers in acknowledging and giving credit where it's due. With that, we'd like to let our utmost gratitude be known to those who've helped us along the way:
+          </p>
+          <p className="pt-2 text-center">
+            City of Boynton Beach (with distinguished praise for the CRA!)
+          </p>
+          <p className="pt-2 text-center">
+            Catoe & Son Plumbing | Flameguard Fire Equipment | LifeSafety Management
+          </p>
+          <p className="pt-2 text-center">
+            Chip Brown of AMJ  |  Challenger Operations Inc
+          </p>
+        </div>
       </div>
+      <StaticImage src="../images/bottom-border.png" alt="" className="w-full" />
     </div>
   </>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,10 @@ import Layout from '../components/layout';
 
 const IndexPage = () => (
   <Layout pageTitle="Boynton Beach, FL">
-    <div className="w-full my-16">
+    <div className="w-full h-screen">
       <div className="max-w-screen-md mx-auto px-8 flex flex-col justify-center h-full">
             
-        <h3 className="mb-8 text-2xl font-medium tracking-wide uppercase">Sometimes, a smaller scale ramen shop just works.</h3>
+        <h3 className="mb-8 text-2xl font-normal tracking-wider uppercase">Sometimes, a smaller scale ramen shop just works.</h3>
         <p className="mb-8">
             In Japan due to its limited space, it's not unusual to find small shops like ours serve the finest ramen. What sets Tokio Ramen apart is the attention to detail in the art of ramen creation, and the fact that ramen is the centerpiece of our offerings. It is customary to taste the broth before enjoying the rest of the bowl to know that you have exquisite quality before you. And be sure to slurp away; we won't judge. Slurping is customary in Japan and is known to actually enhance the flavor that much more.
         </p>
