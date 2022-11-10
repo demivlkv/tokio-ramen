c3129ed664f5c81021a1f4b72a274d7a2bdde79b
Demi Hayashi <demi.h@me.com>
2022-11-10 09:07:40 -0800

    update footer

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
-    const formId = process.env.REACT_APP_CONTACT_FORM_ID;
+    const formId = process.env.GATSBY_GETFORMIO_ID;
     const formUrl = 'https://getform.io/f/' + formId;
 
     // handles form validation
@@ -42,31 +42,31 @@ const Contact = () => {
                     <p>We'd love to hear from you!</p>
 
                     {/* CONTACT FORM */}
-                        <form method="POST" action={formUrl} id="contact-form" className="flex flex-col justify-center max-w-[600px] w-full">
-                            <div class="w-[90%] max-w-screen-md mx-auto">
-                                <label className="block text-[#fe4039] uppercase font-medium">Name:</label>
-                                <input type="text" defaultValue={name} onBlur={handleChange} name="name" className="w-full my-2 p-2 rounded bg-[#f3f3f3] border border-gray-300" />
+                    <form method="POST" action={formUrl} id="contact-form" className="flex flex-col justify-center w-full">
+                        <div class="w-[75%] max-w-xl mx-auto">
+                            <label className="block text-[#fe4039] uppercase font-medium">Name:</label>
+                            <input type="text" defaultValue={name} onBlur={handleChange} name="name" className="w-full my-2 p-2 rounded bg-[#f3f3f3] border border-gray-300 font-medium focus:outline-0 focus:ring-2 focus:ring-[#222]" />
 
-                                <label className="block text-[#fe4039] uppercase font-medium">E-mail:</label>
-                                <input type="email" defaultValue={email} onBlur={handleChange} name="email" className="w-full my-2 p-2 rounded bg-[#f3f3f3] border border-gray-300" />
+                            <label className="block text-[#fe4039] uppercase font-medium">E-mail:</label>
+                            <input type="email" defaultValue={email} onBlur={handleChange} name="email" className="w-full my-2 p-2 rounded bg-[#f3f3f3] border border-gray-300 font-medium focus:outline-0 focus:ring-2 focus:ring-[#222]" />
 
-                                <label className="block text-[#fe4039] uppercase font-medium">Message:</label>
-                                <textarea name="message" defaultValue={message} onBlur={handleChange} rows="4" className="w-full my-2 p-2 rounded bg-[#f3f3f3] border border-gray-300" />
+                            <label className="block text-[#fe4039] uppercase font-medium">Message:</label>
+                            <textarea name="message" defaultValue={message} onBlur={handleChange} rows="4" className="w-full my-2 p-2 rounded bg-[#f3f3f3] border border-gray-300 font-medium focus:outline-0 focus:ring-2 focus:ring-[#222]" />
 
-                                {errorMessage && (
-                                    <div>
-                                        <p className="error-text">{errorMessage}</p>
-                                    </div>
-                                )}
+                            {errorMessage && (
+                                <div>
+                                    <p className="error-text">{errorMessage}</p>
+                                </div>
+                            )}
 
-                                <button type="submit" className="font-medium uppercase tracking-widest group w-[150px] mx-auto flex items-center my-2 pl-8 py-2 border-2 border-[#fe4039] rounded hover:bg-[#fe3f39bb] hover:text-white">
-                                    Submit
-                                    <span className="group-hover:ml-1 duration-300">
-                                        <FiChevronRight className="ml-2" />
-                                    </span>
-                                </button>
-                            </div>
-                        </form>
+                            <button type="submit" className="font-medium uppercase tracking-widest group w-[150px] mx-auto flex items-center my-2 pl-8 py-2 border-2 border-[#fe4039] rounded hover:bg-[#fe3f3978]">
+                                Submit
+                                <span className="group-hover:ml-1 duration-300">
+                                    <FiChevronRight className="ml-2" />
+                                </span>
+                            </button>
+                        </div>
+                    </form>
 
                 </div>
             </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import { FiInstagram } from '@react-icons/all-files/fi/FiInstagram';
+import { FiFacebook } from '@react-icons/all-files/fi/FiFacebook';
 
 const Footer = () => (
     <footer>
-        <div className="relative w-full flex justify-center items-center p-8 bg-[#1b1b1b] border-t border-gray-300 text-white">
+        <div className="fixed z-50 bottom-0 left-0 w-full flex justify-between items-center p-3 bg-[#1b1b1b] border-t border-gray-300 text-white">
+            <div>
+                <StaticImage src="../images/logo-footer.png" alt="" className="w-[40px]" />
+            </div>
 
             <div className="font-normal text-base tracking-wider">
                 <p>© {new Date().getFullYear()} &middot; T<span className="text-[#fe4039]">o</span>kio Ramen. All rights reserved.</p>
+            </div>
+
+            <div className="flex justify-center items-center">
+                <Link to="https://www.instagram.com/tokioramen/">
+                    <button className="h-[35px] inline-flex items-center mr-2 text-white hover:text-[#fe4039]">
+                        <FiInstagram size={20} />
+                    </button>
+                </Link>
+                <Link to="https://www.facebook.com/tokioramenbb">
+                    <button className="h-[35px] inline-flex items-center ml-2 text-white hover:text-[#fe4039]">
+                    <FiFacebook size={20} />
+                    </button>
+                </Link>
             </div>
 
         </div>
