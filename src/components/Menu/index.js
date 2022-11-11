b8ec2167aea03fd0c97c9429d452ae9bdd523e6b
Demi Hayashi <demi.h@me.com>
2022-11-10 17:59:29 -0800

    create data.js; update Menu component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,45 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import menus from './data';
+
+const MenuItem = ({ name, description, price }) => (
+  <div className="w-full flex justify-between">
+    <div>
+      <h4>{name}</h4>
+      <p>{description}</p>
+    </div>
+    <p>{price}</p>
+  </div>
+);
+
+const MenuSection = ({ name, items, image, imageAlt }) => (
+  <div className="mb-8">
+    <h3>{name}</h3>
+    <img src={image} alt={imageAlt} />
+    {items.map(item => (
+      <MenuItem
+        key={item.name}
+        name={item.name}
+        price={item.price}
+        description={item.description}
+      />
+    ))}
+  </div>
+);
+
+const FullMenu = ({ name, sections }) => (
+  <div className="mb-8">
+    <h2>{name}</h2>
+    {sections.map(section => (
+      <MenuSection
+        key={section.name}
+        name={section.name}
+        items={section.items}
+        image={section.image}
+        imageAlt={section.imageAlt}
+      />
+    ))}
+  </div>
+);
 
 const Menu = () => (
   <>
@@ -7,8 +47,9 @@ const Menu = () => (
       <div className="h-full flex flex-col justify-center items-center">
 
         <h2>Menu</h2>
-        <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
+        {menus.map(menu => (
+          <FullMenu key={menu.name} sections={menu.sections} />
+        ))}
 
       </div>
     </div>
