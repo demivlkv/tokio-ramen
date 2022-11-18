18edf36c82a97daecbd480ca8578a4486bc62b83
Demi Hayashi <demi.h@me.com>
2022-11-17 23:01:38 -0800

    update menu items layout

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,62 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ImFire } from '@react-icons/all-files/Im/ImFire';
 import useMenus from '../../hooks/useMenus';
 
 const MenuItem = ({ name, description, price, spicy, categoryList }) => (
-  <section>
-    <div className="mx-auto grid col-auto gap-4">
-      <div className="flex justify-between">
-        <div>
-          <h4 className="text-xl font-medium flex-inline items-center">
-            {name} {spicy === true ? <div className="mx-2 w-[25px] h-[25px] inline-flex justify-center items-center rounded-full bg-[#fe4039] text-white"><ImFire size={16} /></div> : ""}
-          </h4>
-          <p className="py-4">{description}</p>
-        </div>
-        <p className="font-medium">{price}</p>
-      </div>
+  <div className="w-full md:w-[40%] m-4 md:m-8 flex justify-evenly items-start self-start border-l-4 border-gray-100 pl-8 pt-4">
+    <div className="w-full">
+      <h4 className="text-red-700 text-xl font-medium flex-inline items-center">
+        {name} {spicy === true ? <div className="mx-2 w-[25px] h-[25px] inline-flex justify-center items-center rounded-full bg-[#fe4039] text-white"><ImFire size={16} /></div> : ""}
+      </h4>
+      <p className="py-4">{description}</p>
     </div>
-  </section>
+    <p className="text-red-700 text-xl font-medium">{price}</p>
+  </div>
 );
 
-const MenuSection = ({ name, items, image, imageAlt }) => {
-  const sections = ['beverages', 'appetizers', 'ramen', 'ramen toppings', 'udon & soba', 'rice']
-  const [currentSection, setCurrentSection] = useState('beverages');
-
-  return (
+const MenuSection = ({ name, items, image, imageAlt }) => (
   <>
-    <ul className="tracking-widest">
-    {sections.map((section) => (
-      <li className={
-        currentSection.name === name ? 'text-lg text-red-700 font-semibold uppercase' : 'text-blue-700'
-      } key={name} >
-        <span onClick={() => setCurrentSection(section)} className={
-          currentSection === section ? 'mx-2 active font-semibold' : 'mx-2 font-medium hover:border-b-4 hover:border-[#5e7089] transition-all hover:text-white'
-        }>{section}</span>
-        </li>
-        ))}
-      </ul>
+    <h3 className="my-8 text-center text-xl font-medium uppercase tracking-widest">{name}</h3>
+    <div className="flex justify-center items-start">
+      <img src={image} alt={imageAlt} width={450} className="rounded-md drop-shadow-lg" />
+    </div>
 
-      {/* <div>
-    <h3 className="text-lg text-red-700 font-semibold uppercase">{name}</h3>
-    <img src={image} alt={imageAlt} width={500} />
-    {items.map(item => (
-      <MenuItem
-        key={item.name}
-        name={item.name}
-        price={item.price}
-        description={item.description}
-        spicy={item.spicy}
-        categoryList={item.categoryList}
-      />
-    ))};
-  </div> */}
-      </>
-    )
-  };
+    <div className="max-w-screen-lg h-full mx-auto">
+      <div className="flex flex-row flex-wrap justify-center items-center">
+        {items.map(item => (
+          <MenuItem
+            key={item.name}
+            name={item.name}
+            price={item.price}
+            description={item.description}
+            spicy={item.spicy}
+            categoryList={item.categoryList}
+          />
+        ))}
+      </div>
+    </div>
+  </>
+);
 
 const FullMenu = ({ name, sections }) => (
-  <div className="mb-8">
-    <h3 className="text-lg text-blue-700 font-semibold uppercase">{name}</h3>
+  <>
     {sections.map(section => (
       <MenuSection
         key={section.name}
@@ -66,7 +49,7 @@ const FullMenu = ({ name, sections }) => (
         imageAlt={section.imageAlt}
       />
     ))}
-  </div>
+  </>
 );
 
 const Menu = () => {
@@ -74,15 +57,13 @@ const Menu = () => {
 
   return (
     <section id="menu">
-      <div className="w-full h-full">
-        <div className="h-full overflow-y-scroll flex flex-col justify-center items-center">
+      <div className="w-full h-full pt-14 md:pt-16 px-8 md:px-4">
+        <div className="h-full flex flex-col justify-center items-center">
 
           <h2>Menu</h2>
-          <div className="max-w-screen-lg flex justify-center items-center">
             {menus.map(menu => (
-              <FullMenu key={menu.name} sections={menu.sections} />
+              <FullMenu key={menu.name} name={menu.name} sections={menu.sections} />
             ))}
-          </div>
 
         </div>
       </div>
