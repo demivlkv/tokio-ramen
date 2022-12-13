1e209d69ba5463088d55ef3705ca6a7cad336ad6
Demi Hayashi <demi.h@me.com>
2022-12-13 15:51:05 -0800

    fix error in production

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 const NotFoundPage = () => (
   <Layout pageTitle="404: Not Found">
