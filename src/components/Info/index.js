c5962d6e736551eb7fadbb648f045e07066b1fca
Demi Hayashi <demi.h@me.com>
2023-04-11 12:14:25 -0700

    update css for Info component

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -52,64 +52,64 @@ const Info = () => (
             transition={{ duration: 1, delay: 0.5 }}
             viewport={{ once: true }}
           >
-            <div className="inline-flex self-start items-center">
+            <div className="flex self-start items-center">
               <MapPin style={{ color: '#fe4039' }} size={20} className="mr-3" />
               <p className="font-medium uppercase">Location :</p>
             </div>
-            <div className="inline-flex">
+            <div>
               <p>
                 One Boynton Plaza<br />
                 307 E Woolbright Rd<br />
                 Boynton Beach, FL 33435
               </p>
             </div>
-            <div className="inline-flex self-start items-center">
+            <div className="flex self-start items-center">
               <Phone style={{ color: '#fe4039' }} size={20} className="mr-3" />
               <p className="font-medium uppercase">Phone :</p>
             </div>
-            <div className="inline-flex">
+            <div>
               <p>(561) 509-8131</p>
             </div>
             {/* HOURS OF OPERATION */}
-            <div className="inline-flex self-start items-center">
+            <div className="flex self-start items-center">
               <Clock style={{ color: '#fe4039' }} size={20} className="mr-3" />
               <p className="font-medium uppercase">Hours of Operation :</p>
             </div>
             <div></div>
-            <div className="inline-flex justify-end items-center pr-4">
+            <div className="flex justify-end items-center pr-4">
               <p className="font-medium uppercase">Sun</p>
             </div>
-            <div className="flex">
+            <div className="flex items-center">
               <p>12 pm to 7 pm</p>
             </div>
-            <div className="inline-flex justify-end items-center pr-4">
+            <div className="flex justify-end items-center pr-4">
               <p className="font-medium uppercase">Mon</p>
             </div>
-            <div className="flex">
+            <div className="flex items-center">
               <p>CLOSED</p>
             </div>
-            <div className="inline-flex justify-end items-center pr-4">
+            <div className="flex justify-end items-center pr-4">
               <p className="font-medium uppercase">Tues & Thurs</p>
             </div>
-            <div className="flex">
+            <div className="flex items-center">
               <p>12 pm to 10 pm</p>
             </div>
-            <div className="inline-flex justify-end items-center pr-4">
+            <div className="flex justify-end items-center pr-4">
               <p className="font-medium uppercase">Wed</p>
             </div>
-            <div className="flex">
+            <div className="flex items-center">
               <p>12 pm to 10 pm</p>
             </div>
-            <div className="inline-flex justify-end items-center pr-4">
+            <div className="flex justify-end items-center pr-4">
               <p className="font-medium uppercase">Fri</p>
             </div>
-            <div className="flex">
+            <div className="flex items-center">
               <p>12 pm to 10 pm</p>
             </div>
-            <div className="inline-flex justify-end items-center pr-4">
+            <div className="flex justify-end items-center pr-4">
               <p className="font-medium uppercase">Sat</p>
             </div>
-            <div className="flex">
+            <div className="flex items-center">
               <p>12 pm to 10 pm</p>
             </div>
           </motion.div>
