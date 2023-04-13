775a001638de69a4a8db4fcc922119b9534d632e
Demi Hayashi <demi.h@me.com>
2023-04-13 16:33:17 -0700

    minor animation fix in Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -22,6 +22,7 @@ const Layout = ({ pageTitle, children }) => {
           whileInView={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -100 }}
           transition={{ duration: 1, delay: 0.75 }}
+          viewport={{ once: true }}
         >
           {children}
         </motion.main>
